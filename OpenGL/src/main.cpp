bfb305f8ea2130c37b9205b03cb624e479664662
David Sladký <davsladk@gmail.com>
2021-11-13 16:31:31 +0100

    number 5 - attrib pointer

--- OpenGL/src/main.cpp
+++ OpenGL/src/main.cpp
@@ -41,6 +41,9 @@ int main(void)
     glBindBuffer(GL_ARRAY_BUFFER, buffer); // select buffer as array buffer
     glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW); // where to get data
 
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0); // define what is in buffer
+    glDisableVertexAttribArray(0);
+
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
